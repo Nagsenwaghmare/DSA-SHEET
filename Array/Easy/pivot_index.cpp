4db48d5481fbeb09cd44efaa45402ffceb005041
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-10-28 21:08:46 +0530

    Create pivot_index.cpp

---
+++ Array/Easy/pivot_index.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+int pivotIndex(vector<int>& a) {
+        int sum = 0;
+        int n= a.size();
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        int left_sum = 0;
+        int righ_sum = sum;
+        for(int i=0;i<n;i++){
+            righ_sum= (righ_sum-a[i]);
+            if(left_sum==righ_sum)
+                return i;
+            left_sum+=a[i];
+        }
+        
+        return -1;
+    }
