cbeefea096b6aef758fd10a0606501dc9a65ee2f
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-08 02:51:41 +0530

    Create happy_number.cpp

---
+++ Maths/Easy/happy_number.cpp
@@ -0,0 +1,31 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+
+
+
+bool isHappy(int n) {
+        int sum = 0;
+        while(n>0){
+            sum+=pow(n%10,2);
+            n/=10;
+        }
+        if(sum==4)return 0;
+        if(sum!=1) return isHappy(sum);
+        else{
+            return 1;
+        }
+    }
