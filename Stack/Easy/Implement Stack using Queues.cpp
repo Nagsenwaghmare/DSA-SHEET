c303d0056635ab0d346eeb7356fe497f59fad397
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-11 00:38:41 +0530

    Create Implement Stack using Queues.cpp

---
+++ Stack/Easy/Implement Stack using Queues.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+stack <int>s;
+    // MyStack() {
+        
+    // }
+    
+    void push(int x) {
+        s.push(x);
+    }
+    
+    int pop() {
+        int x = s.top();
+        s.pop();
+        return x;
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    bool empty() {
+        return s.empty();
+    }
+
