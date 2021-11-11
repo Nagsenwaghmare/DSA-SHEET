1fadbc07785f7819b59e899c8a7b537a5424b10b
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-12 00:50:24 +0530

    Create String to Integer (atoi).cpp

---
+++ Maths/Medium/String to Integer (atoi).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int myAtoi(string s) {
+        stringstream geek(s);
+        int x = 0;
+        geek>>x;
+        return x;
+    }
+
+
+    
