81d4b9091606da492adf0fe9b90d77ec54990fcd
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-15 01:16:41 +0530

    Create Minimum Remove to Make Valid Parentheses.cpp

---
+++ Strings/Medium/Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string minRemoveToMakeValid(string s) {
+        stack <int> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else if(s[i]==')'){
+                if(!st.empty()){
+                    st.pop();
+                }else{
+                    s[i] = '0';
+                }
+            }
+        }
+        while(!st.empty()){
+         s[st.top()] = '0';
+            st.pop();
+        }
+        string str ="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]!='0'){
+                str+=s[i];
+            }
+        }
+        return str;
+    }
