2c952cb1b5540b7ae881297baccd2ccf633956ed
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-22 21:32:01 +0530

    Update Sort List.cpp

--- Linkedlist/Medium/Sort List.cpp
+++ Linkedlist/Medium/Sort List.cpp
@@ -11,6 +11,58 @@ struct ListNode {
   };
 
 
+//O(1) extra _SPACE
+
+ListNode* midpoint(ListNode* head){
+        if(head == NULL or head->next == NULL){
+            return head;
+        }
+        
+        ListNode *slow = head, *fast = head->next;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+    
+    ListNode* mergeSortedLists(ListNode* a, ListNode* b){
+        if(a == NULL) return b;
+        if(b == NULL) return a;
+        
+        ListNode* temp;
+        if(a->val <= b->val){
+            temp = a;
+            temp->next = mergeSortedLists(a->next, b);
+        }
+        else{
+            temp = b;
+            temp->next = mergeSortedLists(a, b->next);
+        }
+        
+        return temp;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL or head->next == NULL){
+            return head;
+        }
+        
+        ListNode* mid = midpoint(head);
+        ListNode* a = head;
+        ListNode* b = mid->next;
+        
+        mid->next = NULL;
+        
+        a = sortList(a);
+        b = sortList(b);
+        
+        ListNode* temp = mergeSortedLists(a, b);
+        return temp;
+    }
+
+
   ListNode* sortList(ListNode* head) {
         ListNode *temp = head;
         ListNode newhead(0);
