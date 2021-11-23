30e97dc912d945e3fbe147e05d89be82b9325f45
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-23 21:53:20 +0530

    Create Pow(x, n).cpp

---
+++ Recursion/Medium/Pow(x, n).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+double myPow(double x, int n) {
+        if(n==0) return 1;
+        
+        double y = myPow(x,n/2);
+        if(n%2==0){
+            
+            return y*y;
+            
+        }
+        else{
+            return n < 0 ? 1/x*y*y : x*y*y; 
+        }
+    }
