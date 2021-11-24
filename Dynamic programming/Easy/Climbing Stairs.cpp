e3bbc54b92009adf71e052401ff701286ba065ce
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-24 21:30:43 +0530

    Create Climbing Stairs.cpp

---
+++ Dynamic programming/Easy/Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n) {
+        vector<int> dp(n,0);
+        if(n==1) return 1;
+        dp[0] =1;
+        dp[1] = 2;
+        for(int i=2;i<n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
+    }
