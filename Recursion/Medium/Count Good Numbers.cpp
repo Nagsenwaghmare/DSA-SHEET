d685334167699e97f0cd1e7ea352172bce086e9b
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-11-29 15:35:11 +0530

    Create Count Good Numbers.cpp

---
+++ Recursion/Medium/Count Good Numbers.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+
+long long modPow(long long x, long long y, int m = 1000000007)
+{
+    if (y == 0)
+        return 1;
+    auto p = modPow(x, y / 2, m);
+    return p * p * (y % 2 ? x : 1) % m;
+}
+int countGoodNumbers(long long n) {
+    return modPow(5, (n + 1) / 2) * modPow(4, n / 2) % 1000000007;
+}  
