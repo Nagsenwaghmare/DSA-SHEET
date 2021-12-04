31da4e5dfe48f0801b8a294787149379ff418059
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-12-05 01:10:03 +0530

    Update Jump Game II.cpp

--- Dynamic programming/Medium/Jump Game II.cpp
+++ Dynamic programming/Medium/Jump Game II.cpp
@@ -20,3 +20,17 @@ int jump(vector<int>& nums) {
         }
         return dp[n-1];
     }
+
+
+
+    int jump(vector<int>& nums) {
+    int jumps = 0, curEnd = 0, curFarthest = 0;
+	for (int i = 0; i < nums.size() - 1; i++) {
+		curFarthest = max(curFarthest, i + nums[i]);
+		if (i == curEnd) {
+			jumps++;
+			curEnd = curFarthest;
+		}
+	}
+	return jumps;
+    }
