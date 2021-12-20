92a817e6606f112e5229ea106c93c5b5c7350143
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2021-12-21 01:19:52 +0530

    Create Serialize and Deserialize Binary Tree.cpp

---
+++ Tree/Hard/Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+struct TreeNode {
+     int val;
+      TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+
+   string serialize(TreeNode* root) {
+        if (root == NULL) return "#";
+        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+    }
+
+    TreeNode* deserialize(string data) {
+        if (data == "#") return NULL;
+        stringstream s(data);
+        return makeDeserialize(s);
+    }
+    
+    TreeNode* makeDeserialize(stringstream& s) {
+        string str;
+        getline(s, str, ',');
+        if (str == "#") {
+            return NULL;
+        } else {
+            TreeNode* root = new TreeNode(stoi(str));
+            root->left = makeDeserialize(s);
+            root->right = makeDeserialize(s);
+            return root;
+        }
+    }
