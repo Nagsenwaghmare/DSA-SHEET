0b4adda5d28a9cbc86deb2a694d343ec6f5e82af
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-01-02 00:21:02 +0530

    Create Search in a Binary Search Tree.cpp

---
+++ Tree/binary search tree/easy/Search in a Binary Search Tree.cpp
@@ -0,0 +1,29 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode {
+     int val;
+      TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+
+
+TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root) return nullptr;
+        if(root->val==val) return root;
+        else if(root->val>val){
+            return searchBST(root->left,val);
+        }
+        else{
+            return searchBST(root->right,val);
+        }
+        
+    }
