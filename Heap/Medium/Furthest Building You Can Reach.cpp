9cbb2c24511a8faae3cbaa4df09231346e62cbb8
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-01-13 00:45:07 +0530

    Create Furthest Building You Can Reach.cpp

---
+++ Heap/Medium/Furthest Building You Can Reach.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int furthestBuilding(vector<int>& a, int bricks, int ladders) {
+        priority_queue<int>pq;
+        for(int i=0;i<a.size()-1;i++){
+            int d = a[i+1]-a[i];
+            if(d>0){
+                pq.push(-d);
+            }
+            if(pq.size()>ladders){
+                bricks+=pq.top();
+                pq.pop();
+            }
+            if(bricks<0){
+                return i;
+            }
+        }
+        return a.size()-1;
+    }
