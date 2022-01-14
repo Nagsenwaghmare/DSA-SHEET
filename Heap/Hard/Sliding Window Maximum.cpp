9c6e2ad35e1b129e458ac719f4ed30e8c8e54ac0
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-01-15 01:27:33 +0530

    Create Sliding Window Maximum.cpp

---
+++ Heap/Hard/Sliding Window Maximum.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>res;
+        deque<int>dq;
+        for(int i=0;i<nums.size();i++){
+            while(!dq.empty() && dq.front()<i-k+1){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()]<nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1)res.push_back(nums[dq.front()]);
+        }
+        return res;
+    }
