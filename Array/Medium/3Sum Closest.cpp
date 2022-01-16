a404b8e5cb5d128af9596d07316eba26772b0e4d
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-01-16 19:46:58 +0530

    Create 3Sum Closest.cpp

---
+++ Array/Medium/3Sum Closest.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        int closest = nums[0]+nums[1]+nums[2];
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++){
+            int l = i+1;
+            int r = n-1;
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+                if(sum == target)
+                    return sum;
+                else if(sum<target){
+                    if(abs(sum-target)<(abs(closest-target))){
+                        closest = sum;
+                    }
+                    l++;
+                }else{
+                    if(abs(sum-target)<(abs(closest-target))){
+                        closest = sum;
+                    }
+                    r--;
+                }
+            }
+        }
+        return closest;
+    }
