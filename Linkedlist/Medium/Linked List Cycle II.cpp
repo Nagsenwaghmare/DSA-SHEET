9c629f111eaa4729843286be997f4ae3b158c051
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-01-21 14:43:24 +0530

    Update Linked List Cycle II.cpp

--- Linkedlist/Medium/Linked List Cycle II.cpp
+++ Linkedlist/Medium/Linked List Cycle II.cpp
@@ -49,3 +49,10 @@ ListNode *detectCycle(ListNode *head) {
         }
         return nullptr;
     }
+
+
+    // lets suppose when slow travels l1 distance and fast travel l2 distance
+    // time when they meet slow will travel its own distance l1 + distance traveled by l2 hence distance by slow  = l1+l2
+    // and distance traveled by fast will be its own distance l2 +it will travel slow distance l1+l2 +l2+some distance (X)
+    //hence 2(l1+l2) = l1+l2+l2+x;
+    //x = l1  hence when they meet to find the starting point we have to move slow by l1 distance and we will get the starting 
