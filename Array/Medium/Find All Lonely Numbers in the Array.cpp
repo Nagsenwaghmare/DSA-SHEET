2312fefab235f11a606d02bc6e26fa85c185fa24
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-01-23 12:23:56 +0530

    Create Find All Lonely Numbers in the Array.cpp

---
+++ Array/Medium/Find All Lonely Numbers in the Array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> findLonely(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        vector<int>res;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i]-1)==mp.end() && mp.find(nums[i]+1)==mp.end() && mp[nums[i]]==1){
+                res.push_back(nums[i]);
+            }
+        }
+        return res;
+    }
