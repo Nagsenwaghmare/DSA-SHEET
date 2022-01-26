386d38ed9422b7b9352dfc5118a7bb0fd9cccdf0
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-01-26 14:00:47 +0530

    Create Longest Common Subsequence.cpp

---
+++ Strings/Medium/Longest Common Subsequence.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestCommonSubsequence(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
