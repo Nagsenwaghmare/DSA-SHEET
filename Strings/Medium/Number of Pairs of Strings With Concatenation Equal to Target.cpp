6a7b408c29ae2c00d437730953f7e957dfb5c131
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-02-05 19:59:01 +0530

    Create Number of Pairs of Strings With Concatenation Equal to Target.cpp

---
+++ Strings/Medium/Number of Pairs of Strings With Concatenation Equal to Target.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numOfPairs(vector<string>& nums, string target) {
+        int cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    cnt++;
+                }if(nums[j]+nums[i]==target){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
