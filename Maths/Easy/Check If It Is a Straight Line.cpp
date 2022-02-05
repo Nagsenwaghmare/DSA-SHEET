0623b04b36453be9eff3fd26037cb097a3abe614
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-02-05 19:58:12 +0530

    Update Check If It Is a Straight Line.cpp

--- Maths/Easy/Check If It Is a Straight Line.cpp
+++ Maths/Easy/Check If It Is a Straight Line.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int cz = coordinates.size();
-        if (cz == 1) 
-        if (cz == 2) 
+// bool checkStraightLine(vector<vector<int>>& coordinates) {
+//         int cz = coordinates.size();
+//         if (cz == 1) 
+//         if (cz == 2) 
         
-        int x0 = coordinates[0][0], y0 = coordinates[0][1];
-        int dx = coordinates[1][0] - x0, dy = coordinates[1][1] - y0;
+//         int x1 = coordinates[0][0];
+//         int y0 = coordinates[0][1];
+//         int dx = coordinates[1][0]- //x1;
+//         //int dy = coordinates[1][1] - y0;
         
-        for (int i = 1; i < cz; i++) {  
-            int x = coordinates[i][0], y = coordinates[i][1];
-            if (dx * (y - y0) != dy * (x - x0))  
-			    return false;
-        }
-        return true;
-    }
+//         //for (int i = 1; i < cz; i++) {  
+//             int x = coordinates[i][0], y = coordinates[i][1];
+//             if (dx * (y - y0) != dy * (x - x1))  
+// 			    return false;
+//         }
+//         return true;
+//     }
