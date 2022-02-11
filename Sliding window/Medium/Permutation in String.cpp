6670c92701cb6136cd15ae1f09142e45fca2e179
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-02-11 23:01:26 +0530

    Create Permutation in String.cpp

---
+++ Sliding window/Medium/Permutation in String.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkInclusion(string s1, string s2) {
+        vector<int> map_s1(26), map_s2(26);
+		for(char& i1 : s1) 
+			map_s1[i1 - 'a']++;
+		for(int i = 0; i < s2.size(); i++) {
+			map_s2[s2[i] - 'a']++;
+			if(i >= s1.size() - 1) {
+				if(map_s2 == map_s1)  
+					return true;
+				map_s2[s2[i - s1.size() + 1] - 'a']--;
+			}
+		}
+		return false;
+    }
