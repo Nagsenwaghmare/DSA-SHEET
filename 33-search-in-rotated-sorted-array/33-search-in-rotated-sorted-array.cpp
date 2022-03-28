13cfc3bf18a00f5f4abe307b68283895998412c4
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-03-28 10:55:39 +0530

    Time: 3 ms (86.01%), Space: 11.1 MB (29.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]=i;
+        int n = nums.size();
+        int l=0;
+        int r = n-1;
+        int start = 0;
+        while(l<r){
+            int mid  = l+(r-l)/2;
+            if(nums[mid]>nums[r]) l = mid+1;
+            else r = mid;
         }
-        if(mp.find(target)==mp.end()){
-            return -1;
+        start = l;
+        l =0;
+        r = n-1;
+        if(target>=nums[start] && target<=nums[r]){
+            l = start;
+        }else{
+            r = start;
         }
-        return mp[target];
+        while(l<=r){
+            int mid= l+(r-l)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) r = mid-1;
+            else l = mid+1;
+        }
+        return -1;
     }
 };
