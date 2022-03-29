cd8e5471e40a3c862731376a201496f4af5a358e
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-03-29 09:37:00 +0530

    Time: 203 ms (31.48%), Space: 61.3 MB (26.65%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto i:nums){
-            mp[i]++;
-        }
-        for(auto it:mp){
-            if(it.second>=2)return it.first;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]) return nums[i];
         }
         return -1;
     }
