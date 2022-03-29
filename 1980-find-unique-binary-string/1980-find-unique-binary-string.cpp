01bfed2a0ee2a89bfdae20d052dd63c42f89f0fc
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-03-29 12:39:02 +0530

    Time: 4 ms (70.12%), Space: 10.1 MB (90.73%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
+    string backtrack(string s,int n,vector<string>&nums){
+        if(s.size()==n){
+            if(find(nums.begin(),nums.end(),s)==nums.end()) return s;
+            else return "";
+        }
+        string k;
+        
+        s.push_back('1');
+        k=backtrack(s,n,nums);
+        if(k!="") return k;
+        s.pop_back();
+        
+        s.push_back('0');
+        k = backtrack(s,n,nums);
+        if(k!="") return k;
+        s.pop_back();
+        
+        return  "";
+}
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<int>s;
-        for(auto i:nums){
-            s.insert(stoi(i,nullptr,2));
-        }
-        int x = 0;
-        while(true){
-            if(s.find(x)==s.end()){
-                return bitset<16>(x).to_string().substr(16-nums.size());
-            }
-            x++;
-        }
-        return "";
+
+        string s = "";
+        int n = nums[0].size();
+        return backtrack(s,n,nums);
     }
 };
