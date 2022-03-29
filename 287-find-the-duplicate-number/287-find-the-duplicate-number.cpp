363cb572bebaf70c78dbca9a39ce6e319a4f9ff2
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-03-29 10:09:40 +0530

    Time: 162 ms (54.11%), Space: 61.3 MB (61.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]) return nums[i];
+        int l = 1;
+        int r = nums.size()-1;
+        
+        // in this solution we are using binary search where we gonna count the number of elements which
+        // are greater than the middle element if count of those is greater than mid then our repeated 
+        // element lies in the left side 
+        while(l<r){
+            int mid = l+(r-l)/2;
+            int cnt=0;
+            for(auto k:nums){
+                if(k<=mid)cnt++;
+            }
+            if(cnt>mid)r = mid;
+            else l = mid+1;
         }
-        return -1;
+        return l;
     }
 };
