656b24a0f3a4df49aaee9fe971560f104ac18683
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-03-30 16:36:34 +0530

    Time: 4 ms (77.73%), Space: 9.6 MB (52.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -6,8 +6,13 @@ public:
         for(int i=0;i<m;i++){
             if(matrix[i][0]<=target && matrix[i][n-1]>=target)
             {
-                for(int j=0;j<n;j++){
-                    if(matrix[i][j]==target)return true;
+                int l =0;
+                int r = n-1;
+                while(l<=r){
+                    int mid = l+(r-l)/2;
+                    if(matrix[i][mid]==target)return true;
+                    else if(matrix[i][mid]<target)l=mid+1;
+                    else if(matrix[i][mid]>target)r = mid-1;
                 }
             }
         }
