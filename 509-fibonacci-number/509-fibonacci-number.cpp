d9bc0b4d153ed4d89262b4621d8e857ed54169cb
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-03-31 14:56:34 +0530

    Time: 2 ms (57.64%), Space: 5.9 MB (79.99%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 ) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        if(n==0 ||n==1)return n;
+        int dp[n+1];
+        fill(dp,dp+(n+1),0);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<(n+1);i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
