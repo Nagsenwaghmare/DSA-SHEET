89140abc530c3ef858be771a381b8065854a6a4d
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-04-11 14:22:35 +0530

    Time: 162 ms (27.58%), Space: 14.4 MB (67.50%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,32 +2,15 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        int dp[n+1][amount+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<amount+1;j++){
-                if(j==0)dp[i][j]=0;
-                if(i==0)dp[i][j] = INT_MAX-1;
-            }
-        }
-        for(int j=1;j<amount+1;j++){
-            if(j%coins[0]==0){
-                dp[1][j]=j/coins[0];
-            }else{
-                dp[1][j]=INT_MAX-1;
-            }
-        }
-        for(int i=2;i<n+1;i++){
-            for(int j=1;j<amount+1;j++){
-                if(coins[i-1]<=j){
-                    dp[i][j] = min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
-                }else{
-                    dp[i][j] = dp[i-1][j];
+        vector<int>dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(auto coin:coins){
+                if(coin<=i){
+                    dp[i] = min(dp[i],1+dp[i-coin]);
                 }
             }
         }
-        if(dp[n][amount]==INT_MAX-1){
-            return -1;
-        }
-        return dp[n][amount];
+        return (dp[amount]>amount?-1:dp[amount]);
     }
 };
