e0c2812f0788409ccb3e72b754f6135605119510
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-04-25 11:08:08 +0530

    Time: 376 ms (100.00%), Space: 88.9 MB (20.00%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-        vector<int> start, end;
-        for (auto& t : flowers)
-            start.push_back(t[0]), end.push_back(t[1]);
-        sort(start.begin(), start.end());
-        sort(end.begin(), end.end());
-        vector<int> res;
-        for (int t : persons) {
-            int started = upper_bound(start.begin(), start.end(), t)-start.end() ;
-            int ended = lower_bound(end.begin(), end.end(), t) -end.end();
-            cout<<started<<" "<<ended<<"\n";
-            res.push_back(started - ended);
+        vector<int>s,e,res;
+        for(auto i:flowers){
+            s.push_back(i[0]);
+            e.push_back(i[1]);
+        }
+        sort(s.begin(),s.end());
+        sort(e.begin(),e.end());
+        for(auto i:persons){
+            int start = upper_bound(s.begin(),s.end(),i)-s.begin();
+            int end  = lower_bound(e.begin(),e.end(),i)-e.begin();
+            res.push_back(start-end);
         }
         return res;
     }
