c77623bbd90dad7dbff56108bbb1b68479e24eca
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-06-29 01:11:23 +0530

    Time: 22 ms (19.25%), Space: 10.5 MB (55.08%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& rooms,int root,vector<bool>&visited){
+        visited[root] = true;
+        for(auto i:rooms[root]){
+            if(!visited[i]){
+                dfs(rooms,i,visited);
+            }
+        }
+        
+    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-        vector<int>visited(n,0);
-        queue<int>q;
-        q.push(0);
-        visited[0] = 1;
-        int count =0;
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(auto i:rooms[curr]){
-                if(visited[i]==0){
-                    q.push(i);
-                    visited[i] = 1;
-                }
-            }
-            count++;
+        vector<bool>visited(n,false);
+        dfs(rooms,0,visited);
+        for(auto i:visited){
+            if(!i)return false;
         }
-        return count==n;
+        return true;
     }
 };
