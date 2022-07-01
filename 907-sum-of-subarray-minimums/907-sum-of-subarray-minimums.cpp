3faa4c4e44f843a30d48fcf4f2c74382366e8831
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-07-02 03:11:27 +0530

    Time: 145 ms (47.40%), Space: 43.9 MB (23.79%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-  int sumSubarrayMins(vector<int>& A) {
-    stack<pair<int, int>> in_stk_p, in_stk_n;
-    // left is for the distance to previous less element
-    // right is for the distance to next less element
-    vector<int> left(A.size()), right(A.size());
-		
-    //initialize
-    for(int i = 0; i < A.size(); i++) left[i] =  i + 1;
-    for(int i = 0; i < A.size(); i++) right[i] = A.size() - i;
-		
-    for(int i = 0; i < A.size(); i++){
-      // for previous less
-      while(!in_stk_p.empty() && in_stk_p.top().first > A[i]) in_stk_p.pop();
-      left[i] = in_stk_p.empty()? i + 1: i - in_stk_p.top().second;
-      in_stk_p.push({A[i],i});
-			
-      // for next less
-      while(!in_stk_n.empty() && in_stk_n.top().first > A[i]){
-        auto x = in_stk_n.top();in_stk_n.pop();
-        right[x.second] = i - x.second;
-      }
-      in_stk_n.push({A[i], i});
+  int sumSubarrayMins(vector<int>& a) {
+    stack<pair<int,int>>p,n;
+    vector<int> left(a.size()), right(a.size());
+    for(int i = 0; i < a.size(); i++) left[i] =  i + 1;
+    for(int i = 0; i < a.size(); i++) right[i] = a.size() - i;
+    for(int i = 0; i < a.size(); i++){
+        while(!p.empty() && a[p.top().second]>a[i]){
+              p.pop();
+        }
+        left[i] = p.empty()?i+1:i-p.top().second;
+        p.push({a[i],i});
+        while(!n.empty() && n.top().first>a[i]){
+            auto x = n.top();n.pop();
+            right[x.second] = i-x.second;
+        }
+        n.push({a[i],i});
     }
 
     long ans = 0;
     long mod = (int)1e9 + 7;
-    for(int i = 0; i < A.size(); i++) {
-    ans = (ans +(long) A[i] * left[i] * right[i]) % mod;
+    for(int i = 0; i < a.size(); i++) {
+    ans = (ans +(long) a[i] * left[i] * right[i]) % mod;
     }
     return (int)ans;
   }
