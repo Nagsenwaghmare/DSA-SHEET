6ff4cddc9ddbe8dc79631cfb3650f48140269008
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-07-15 01:07:59 +0530

    Time: 3 ms (62.94%), Space: 8.5 MB (23.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int a[3] = {0};
+        
+        for(auto i:nums){
+            a[i]++;    
+        }
+        vector<int>res;
+        while(a[0]--){
+            res.push_back(0);
+        }
+        while(a[1]--){
+            res.push_back(1);
+        }
+        while(a[2]--){
+            res.push_back(2);
+        }
+        nums = res;
     }
 };
