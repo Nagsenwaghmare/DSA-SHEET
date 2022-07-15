00863a4d1ade330dadae2bda090ced08af7b53de
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-07-16 02:23:08 +0530

    Time: 8 ms (39.07%), Space: 8.7 MB (99.41%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if(nums.size()==1) 
+        {
+            return 0;
+        }
+        for(int i=1;i<nums.size()-1;i++)
+        {
+            if(nums[i-1] < nums[i] && nums[i] > nums[i+1])
+            {
+                return i;
+            }
+        }
+        if(nums[nums.size()-1] > nums[nums.size()-2]) 
+        {
+            return nums.size()-1;
+        }
+        return 0;
+    }
+};
