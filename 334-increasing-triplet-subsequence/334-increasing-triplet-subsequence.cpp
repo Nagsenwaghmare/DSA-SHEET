e53065ef318f796e9d0df2b1abef8df257cfb14b
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-07-16 16:54:53 +0530

    Time: 131 ms (25.62%), Space: 65.9 MB (11.69%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         int n = nums.size();
-        int MIN = -2147483648;   //-2^31
-        int MAX = 2147483647;   // 2^31 - 1
         if(n==3){
-            if(nums[0] < nums[1] && nums[1] < nums[2])  return true;
-            return false;
-        }    
-        vector<int> left(n, MAX), right(n, MIN);
-        // for each i, finding min on it's left, not including it
-        for(int i=1; i<=n-2; i++){
-            left[i] = min(left[i-1], nums[i-1]);
+            if(nums[0]<nums[1] && nums[1]<nums[2])return true;
+            else return false;
         }
-        // for each i, finding max on it's right, not including it
-        for(int j=n-2; j>=1; j--){
-            right[j] = max(right[j+1], nums[j+1]);
+        int left[n];
+        int right[n];
+        left[0] = nums[0];
+        right[n-1] = nums[n-1];
+        for(int i=1;i<n;i++){
+            left[i] =min(left[i-1],nums[i]); 
         }
-        for(int i=1; i<=n-2; i++){
-            if(left[i]<nums[i] && nums[i]<right[i]) return true;
+        for(int i=n-2;i>=0;i--){
+            right[i] = max(right[i+1],nums[i]);
         }
-        return false;        
+        for(int i=1;i<n-1;i++){
+            if(nums[i]>left[i] && nums[i]<right[i]){
+                return true;
+            }
+            
+        }
+        return false;
     }
 };
