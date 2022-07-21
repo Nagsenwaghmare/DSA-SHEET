e8575827dc44c996c882bc5366aa4cb0df3557e0
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-07-21 13:33:33 +0530

    Time: 141 ms (33.32%), Space: 50.1 MB (24.80%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        int Map[26] = {0};
-    for(int i = 0; i < s.size(); i++){
-        Map[s[i]-'a']++;
-    }
-    
-    int idx =0;
-    while(idx <s.size() && Map[s[idx]-'a'] >= k)    idx++;
-    if(idx == s.size()) return s.size();
-    
-    int left = longestSubstring(s.substr(0 , idx) , k);
-    int right = longestSubstring(s.substr(idx+1) , k);
-    
-    return max(left, right);
+        int count[26] = {0};
+        for(int i=0;i<s.size();i++){
+            count[s[i]-'a']++;
+        }
+        int idx =0;
+        while(idx<s.size() && count[s[idx]-'a']>=k)idx++;
+        if(idx==s.size())return s.size();
+        
+        int left = longestSubstring(s.substr(0,idx),k);
+        int right = longestSubstring(s.substr(idx+1),k);
+        return max(left,right);
     }
 };
