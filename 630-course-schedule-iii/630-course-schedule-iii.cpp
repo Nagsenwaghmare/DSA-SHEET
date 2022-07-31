983da68fb6ece10b8ec39e63fe46b69fb6624477
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-08-01 00:04:08 +0530

    Time: 548 ms (26.14%), Space: 63.4 MB (41.64%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    static bool cmp(vector<int>&a,vector<int>&b){
+        return a[1]<b[1];
+    }
     int scheduleCourse(vector<vector<int>>& courses) {
-         if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
-        });
-        priority_queue<int> q;
-        int sum = 0;
-        for(auto i : courses) {
-            sum += i[0];
-            q.push(i[0]);
-            if(sum > i[1]) {
-                sum -= q.top();
-                q.pop();
+        int n = courses.size();
+        sort(courses.begin(),courses.end(),cmp);
+        int sum=0;
+        priority_queue<int>pq;
+        for(auto i:courses){
+            sum+=i[0];
+            pq.push(i[0]);
+            if(sum>i[1]){
+                sum-=pq.top();
+                pq.pop();
             }
         }
-        return q.size();
+        return pq.size();
     }
 };
