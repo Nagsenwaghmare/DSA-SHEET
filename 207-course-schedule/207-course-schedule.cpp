9b9e4ffdb72e21495bf2a0e7eeb29bb8fb1ff91f
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-07-31 21:20:10 +0530

    Time: 52 ms (17.76%), Space: 14.1 MB (50.39%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -3,26 +3,27 @@ public:
     bool canFinish(int n, vector<vector<int>>& p) {
         vector<vector<int>>course(n);
         vector<int>indegree(n,0);
-        for(auto kl:p){
-            course[kl[1]].push_back(kl[0]);
-            indegree[kl[0]]++;
+        for(auto i:p){
+            course[i[0]].push_back(i[1]);
+            // indegree[i[0]]++;
+            indegree[i[1]]++;
         }
         queue<int>q;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<indegree.size();i++){
             if(indegree[i]==0)q.push(i);
         }
-        int cnt =0;
+        int cnt=0;
         while(!q.empty()){
-            int curr =q.front();
+            int curr = q.front();
             q.pop();
-            cnt++;
-            for(auto v:course[curr]){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    q.push(v);
+            for(auto i:course[curr]){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.push(i);
                 }
             }
+            cnt++;
         }
-        return cnt ==n;
+        return cnt==n;
     }
 };
