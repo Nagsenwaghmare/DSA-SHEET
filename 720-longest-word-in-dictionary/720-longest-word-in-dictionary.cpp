542760f249f6e0da7414f4104e3868d5efee4e09
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-08-03 01:48:16 +0530

    Time: 68 ms (72.62%), Space: 32.9 MB (26.88%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
@@ -1,13 +1,49 @@
+struct TrieNode{
+    bool isend;
+    TrieNode *child[26];
+    TrieNode(){
+        isend = false;
+        for(int i=0;i<26;i++){
+            child[i] = nullptr;
+        }
+    }
+};
+
+
+
 class Solution {
 public:
+    void insert(TrieNode *root,string str){
+        TrieNode * curr = root;
+        for(auto i:str){
+            if(curr->child[i-'a']==nullptr){
+                curr->child[i-'a'] = new TrieNode();
+            }
+            curr = curr->child[i-'a'];
+        }
+        curr->isend = true;
+    }
+    bool search(TrieNode *root,string s){
+        TrieNode *curr = root;
+        int cnt=0;
+        for(auto k:s){
+            
+            curr =curr->child[k-'a'];
+            if(curr->isend==true)++cnt;
+        }
+        return cnt==s.size();
+    }
     string longestWord(vector<string>& words) {
+        TrieNode *root = new TrieNode();
+        for(auto i:words){
+            insert(root,i);
+        }
         string ans = "";
-        set<string>st;
-        sort(words.begin(),words.end());
-        for(auto w:words){
-            if(w.size()==1 || st.find(w.substr(0,w.size()-1))!=st.end()){
-                ans = w.size()>ans.size()?w:ans;
-                st.insert(w);
+        for(auto s:words){
+             if(ans.size() > s.size()) continue;
+            if(search(root,s)){
+                if(ans.size() == s.size()) ans = min(s, ans);
+                else ans = s;
             }
         }
         return ans;
