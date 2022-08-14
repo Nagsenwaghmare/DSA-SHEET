5fda2d6c32d982f8e8609ec66956328805fd97fa
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-08-14 22:59:09 +0530

    Time: 25 ms (6.70%), Space: 14.1 MB (57.78%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x =0;
+        for(auto i:operations){
+            if(i=="++X"){
+                ++x;
+            }else if(i=="X++"){
+                x++;
+            }else if(i=="X--"){
+                x--;
+            }else{
+                --x;
+            }
+        }
+        return x;
+    }
+};
