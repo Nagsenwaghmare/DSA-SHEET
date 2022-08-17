f4a9ac3442a0eb72669110a46404990e130e1866
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-08-17 21:34:06 +0530

    Time: 18 ms (72.82%), Space: 10.3 MB (87.82%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int ans=1;
-        int n = nums.size();
-        // dp[i] represent LIS ending at index i
-//         hence we check whether there is any index smaller than i that is we check for j
-//         and we extend the LIS at j by 1
-        vector<int>dp(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i] = max(dp[i],dp[j]+1);
-                    ans = max(ans,dp[i]);
-                }
-            }
+        int len =0;
+        for(auto curr:nums){
+            if(len==0 || nums[len-1]<curr)nums[len++]=curr;
+            else *lower_bound(begin(nums),begin(nums)+len,curr)=curr;
         }
-        return ans;
+        return len;
     }
 };
