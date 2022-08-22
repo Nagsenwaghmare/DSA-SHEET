e4181657a317a489627d027dfcdf237461613837
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-08-23 00:34:39 +0530

    Time: 39 ms (11.89%), Space: 19.7 MB (96.91%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0;
+        int  r = n-1;
+        int leftmax =0;
+        int rightmax =0;
+        int ans=0;
+        while(l<=r){
+            leftmax = max(leftmax,height[l]);
+            rightmax = max(rightmax,height[r]);
+            if(leftmax<rightmax){
+                ans+=(leftmax-height[l]);
+                l++;
+            }else{
+                ans+=(rightmax-height[r]);
+                r--;
+            }
+        }
+        return ans;
+    }
+};
