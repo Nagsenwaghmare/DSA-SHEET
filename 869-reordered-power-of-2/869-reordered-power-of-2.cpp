08fbc08385637a6626aac7952012d8c0101acb25
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-08-26 12:18:37 +0530

    Time: 3 ms (73.16%), Space: 6.2 MB (55.65%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-     bool reorderedPowerOf2(int N) {
-        long c = counter(N);
-         cout<<c<<" ";
-        for (int i = 0; i < 32; i++)
-        {
-            cout<<(1 << i)<<" ";
-            if (counter(1 << i) == c) return true;
+    
+    bool reorderedPowerOf2(int n) {
+        vector<int>v(10,0);
+        while(n>0){
+            v[n%10]++;
+            n/=10;
+        }
+        for(auto i:v){
+            cout<<i<<" ";
+        }
+        for(int i=0;i<31;i++){
+            vector<int>temp(10,0);
+            int k = pow(2,i);
+            while(k>0){
+                temp[k%10]++;
+                k/=10;
+            }
+            if(temp==v)return true;
         }
         return false;
-    }
-
-    long counter(int N) {
-        long res = 0;
-        for (; N; N /= 10) res += pow(10, N % 10);
-        return res;
     }
 };
