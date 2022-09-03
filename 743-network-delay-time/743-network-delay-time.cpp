bf8b27c6d579a0ea99ecf705a64237c9bd476537
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-09-03 14:56:10 +0530

    Time: 2901 ms (5.01%), Space: 275.5 MB (5.11%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
-    int dijkstra(vector<pair<int,int>>adj[],int n,int source){
-        vector<int>timetaken(n,INT_MAX);
-        vector<bool>visited(n,false);
-        timetaken[source] = 0;
-        for(int i=0;i<n;i++){
-            int src =-1;
-            int mini = INT_MAX;
-            for(int j=0;j<n;j++){
-                if(visited[j]==false && timetaken[j]<mini ){
-                    mini = timetaken[j];
-                    src = j;
-                    
-                }
-            }
-            if(src==-1)return -1;
-            visited[src] = true;
-            for(auto child:adj[src]){
-                if(visited[child.first] ==false && timetaken[child.first]>timetaken[src]+child.second){
-                    timetaken[child.first]=timetaken[src]+child.second;
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        vector<int> dist(N + 1, INT_MAX);
+        dist[K] = 0;
+        for (int i = 0; i < N; i++) {
+            for (vector<int> e : times) {
+                int u = e[0], v = e[1], w = e[2];
+                if (dist[u] != INT_MAX && dist[v] > dist[u] + w) {
+                    dist[v] = dist[u] + w;
                 }
             }
         }
-        int maxi = INT_MIN;
-        for(auto k:timetaken){
-            maxi = max(maxi,k);
-        }
-        return maxi;
-    }
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        int len = times.size();
-        vector<pair<int,int>>adj[n];
-        for(int i=0;i<len;i++){
-            adj[times[i][0]-1].push_back(make_pair(times[i][1]-1,times[i][2]));
-        }
-        return dijkstra(adj,n,k-1);
+
+        int maxwait = 0;
+        for (int i = 1; i <= N; i++)
+            maxwait = max(maxwait, dist[i]);
+        return maxwait == INT_MAX ? -1 : maxwait;
     }
 };
