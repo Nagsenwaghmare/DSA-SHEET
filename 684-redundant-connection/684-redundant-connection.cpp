7424647e7308862d601d9859d9c0ce287759a4c6
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-09-10 15:54:02 +0530

    Time: 8 ms (81.81%), Space: 8.8 MB (69.78%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,23 +1,28 @@
+class DSU {
+    vector<int> par, rank;
+public:
+    DSU(int n) : par(n), rank(n) {
+        iota(begin(par), end(par), 0);
+    }
+    int find(int x) {
+        if(x == par[x]) return x;                       // x is itself the parent of this component
+        return par[x] = find(par[x]);                   // update parent of x before returning for each call
+    }
+    bool Union(int x, int y) {
+        int xp = find(x), yp = find(y);                 // find parents of x and y, i.e, representatives of components that x and y belong to
+        if(xp == yp) return false;                      // x and y already belong to same component - not possible to union
+        if(rank[xp] > rank[yp]) par[yp] = xp;      // union by rank - join smaller ranked to bigger one
+        else if(rank[yp] > rank[xp]) par[xp] = yp;
+        else par[xp] = yp, rank[yp]++;                  // same rank - join either to other and increment rank of final parent
+        return true;
+    }
+};
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&adj,vector<int>&vis,int curr,int parent=-1){
-        if(vis[curr])return true;
-        vis[curr]=true;
-        for(auto i:adj[curr]){
-            if(i!=parent && dfs(adj,vis,i,curr))return true;
-        }
-        return false;
-    }
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<vector<int>>adj(n+1);
-        vector<int>vis(n+1);
-        for(auto i:edges){
-            fill(begin(vis),end(vis),false);
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-            if(dfs(adj,vis,i[0]))return i;
-        }
-        return {};
+    vector<int> findRedundantConnection(vector<vector<int>>& e) {
+        DSU ds(size(e) + 1);
+        for(auto& E : e) 
+            if(!ds.Union(E[0], E[1])) return E;	// not possible to union - adding this edge was causing the cycle
+        return { };    // un-reachable
     }
 };
