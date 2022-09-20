2fbee24d62a2fd6d0fd7fbdea8eeb1a5d6ac339f
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-09-20 10:04:09 +0530

    Time: 357 ms (75.54%), Space: 16.1 MB (81.84%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -17,10 +17,6 @@ public:
                 }else{
                     dp[i][j]=0;
                 }
-            }
-        }
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
                 res = max(res,dp[i][j]);
             }
         }
