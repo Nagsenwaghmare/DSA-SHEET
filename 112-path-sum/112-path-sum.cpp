8b01a1122033cd228247bf58080f98fa533dd965
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-09-24 11:14:29 +0530

    Time: 65 ms (5.79%), Space: 35.3 MB (38.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -11,28 +11,26 @@
  */
 class Solution {
 public:
-    void help(TreeNode * root,vector<int> temp,vector<vector<int>>&res){
+    void pathsum(TreeNode * root,vector<vector<int>>&res,vector<int>temp,int k){
         if(!root)return;
         temp.push_back(root->val);
-        if(root->left==nullptr && root->right== nullptr){
+        if(root->left==nullptr && root->right==nullptr){
             res.push_back(temp);
-            return ;
+            return;
         }
-        help(root->left,temp,res);
-        help(root->right,temp,res);
+        pathsum(root->left,res,temp,k);
+        pathsum(root->right,res,temp,k);
     }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>>res;
+    bool hasPathSum(TreeNode* root, int k) {
+        if(!root)return false;
         vector<int>temp;
-        help(root,temp,res);
+        vector<vector<int>>res;
+        pathsum(root,res,temp,k);
         for(auto i:res){
             int sum=0;
-            for(auto j:i){
-                cout<<j<<" ";
-                sum+=j;
-            }
-            cout<<"\n";
-            if(sum==targetSum)return true;
+            sum = accumulate(begin(i),end(i),sum);
+            cout<<sum<<" ";
+            if(sum==k)return true;
         }
         return false;
     }
