eb3eb20770cc05bd224370e14ec0630a0004dea7
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-09-24 13:03:28 +0530

    Time: 3 ms (36.76%), Space: 6 MB (67.23%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -1,10 +1,35 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        auto str = to_string(n);
-        next_permutation(begin(str),end(str));
-        auto k = stoll(str);
-        if(k>INT_MAX || k<=n)return -1;
-        return k;
+        string s = to_string(n);
+        int i;
+        int j;
+        for(i=s.size()-2;i>=0;i--){
+            if(s[i]<s[i+1])break;
+        }
+        if(i==-1)return -1;
+        for(j=s.size()-1;j>i;j--){
+            if(s[j]>s[i])break;
+        }
+        swap(s[i],s[j]);
+        reverse(s.begin()+i+1,s.end());
+        return stoll(s)>INT_MAX?-1:stoll(s);
     }
 };
+
+// class Solution {
+// public:
+//     int nextGreaterElement(int n) {
+//    string s = to_string(n);
+//         int i, j;
+        
+//         for(i=s.size() - 2; i>=0; i--) if(s[i] < s[i + 1]) break;
+//         if(i == -1) return -1;
+        
+//         for(j=s.size()-1; j>i; j--) if(s[j] > s[i]) break;
+//         swap(s[i], s[j]);
+//         reverse(s.begin() + i + 1, s.end());
+        
+//         return stol(s) > INT_MAX ? -1 : stol(s);
+//     }
+// };
