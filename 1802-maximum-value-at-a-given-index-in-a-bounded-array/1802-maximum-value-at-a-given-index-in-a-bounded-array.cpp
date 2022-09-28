30a0a0013b7c0358bc1dbc3c163161ee402e59db
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-09-28 12:48:00 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (72.61%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int maxValue(int n, int i, int maxSum) {
-    int l = 0, r = maxSum, j = n - i - 1;
-    while (l < r) {
-        long m = (l + r + 1) / 2;
-        auto need = m * m - ((m > i ? (m - i - 1) * (m - i) : 0)
-            + (m > j ? (m - j - 1) * (m - j) : 0)) / 2;
-        if (need <= maxSum - n)
-            l = m;
-        else
-            r = m - 1;
+    int maxValue(int n, int index, int maxSum) {
+        int lo = 1;
+        int hi = maxSum;
+        int l = index; // no of empty spaces to fill on left
+        int r = n-index-1;// no of empty spaces to fill on right
+        long res=0;
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            long sum=0;
+            long ls=0,rs=0;
+            long m = mid-1;
+            if(r<=m)
+                rs = (m*(m+1))/2-((m-r)*(m-r+1))/2;
+            else
+                rs = (m*(m+1))/2+(r-m);
+            if(l<=m)
+                ls = (m*(m+1))/2-((m-l)*(m-l+1))/2;
+            else
+                ls = (m*(m+1))/2+(l-m);
+            sum = ls+rs+mid;
+            
+            if(sum<=maxSum){
+                res = mid;
+                lo = mid+1;
+            }else
+                hi=mid-1;
+        }
+        return res;
     }
-    return l + 1;
-}
 };
