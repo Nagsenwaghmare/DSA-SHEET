5d2afe307e8a697a671735c348152ed835e3d4f2
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-10-22 16:49:29 +0530

    Time: 165 ms (55.15%), Space: 23.6 MB (26.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& a) {
+    vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>res;
-        int n = a.size();
-        sort(begin(a),end(a));
-        for(int i=0;i<n-2;i++){
-            if(i==0 || a[i]!=a[i-1]){
-                int l = i+1;
-                int r = n-1;
-                int target = 0-(a[i]);
-                while(l<r){
-                    if(a[l]+a[r]==target){
-                        vector<int>temp;
-                        temp.push_back(a[i]);
-                        temp.push_back(a[l]);
-                        temp.push_back(a[r]);
-                        res.push_back(temp);
-                        while(l<r && a[l]==a[l+1])l++;
-                        while(l<r && a[r]==a[r-1])r--;
-                        l++;
-                        r--;
-                    }else if((a[l]+a[r])<target){
-                        l++;
-                    }else if((a[l]+a[r])>target){
-                        r--;
-                    }
+        std::sort(begin(nums),end(nums));
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int l= i+1;
+            int r = n-1;
+            int target  = -nums[i];
+            while(l<r){
+                int sum = nums[l]+nums[r];
+                if(sum==target){
+                    vector<int>temp;
+                    temp.push_back(nums[l]);
+                    temp.push_back(nums[r]);
+                    temp.push_back(nums[i]);
+                    res.push_back(temp);
+                    while(l<r && nums[l]==temp[0])l++;
+                    while(l<r && nums[r]==temp[1])r--;
+                }if(sum>target){
+                    r--;
+                }else if(sum<target){
+                    l++;
                 }
             }
+            while(i+1<n && nums[i]==nums[i+1])i++;
         }
         return res;
     }
