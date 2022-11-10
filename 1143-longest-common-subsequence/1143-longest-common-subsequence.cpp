c77e4a10f7e8bc25d7746ebe9051e87ae0e22208
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-11-10 18:43:49 +0530

    Time: 804 ms (5.10%), Space: 13.1 MB (24.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,20 +3,21 @@ public:
     int longestCommonSubsequence(string s1, string s2) {
         int m = s1.size();
         int n = s2.size();
-        int dp[m+1][n+1];
-        for(int i=0;i<m+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(i==0 || j==0) dp[i][j]=0;
-            }
-        }
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
                 if(s1[i-1]==s2[j-1]){
-                    dp[i][j] = dp[i-1][j-1]+1;
+                    dp[i][j] = 1+ dp[i-1][j-1];
                 }else{
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
+        }
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<"\n";
         }
         return dp[m][n];
     }
