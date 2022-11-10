a000dbedb138fe16449ea602261f3186cad0b1f2
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-11-10 18:44:15 +0530

    Time: 53 ms (40.13%), Space: 13 MB (46.40%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -13,12 +13,6 @@ public:
                 }
             }
         }
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
         return dp[m][n];
     }
 };
