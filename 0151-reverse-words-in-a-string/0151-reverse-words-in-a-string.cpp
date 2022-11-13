a76ea940af2c67000783feac1e881a1eab862c43
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-11-13 07:54:19 +0530

    Time: 36 ms (9.26%), Space: 46.9 MB (7.05%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+    int n = s.length();
+    if(n==0) return s;
+        stack<string>st;
+        for(int i=0;i<n;i++){
+            if(s[i]==' ')continue;
+                string temp;
+                while(s[i]!=' ' && i<n){
+                    temp+=s[i++];
+                }
+                st.push(temp);
+        }
+        string word = "";
+        while(!st.empty()){
+            word = word+st.top();
+            st.pop();
+            if(!st.empty())word+=" ";
+        }
+        return word;
+    }
+};
