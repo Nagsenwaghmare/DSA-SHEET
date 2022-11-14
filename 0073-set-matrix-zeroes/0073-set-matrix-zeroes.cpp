1d1a5cf106a27f7c98ede6c3272e6642dfa1144a
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-11-14 13:47:17 +0530

    Time: 18 ms (85.55%), Space: 13.3 MB (20.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        set<int>row,col;
-        int m =matrix.size();
-        int n =matrix[0].size();
+    void setZeroes(vector<vector<int>>& a) {
+        bool row = false;
+        bool col = false;
+        int m = a.size();
+        int n = a[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+                if(a[i][j]==0){
+                    if(i==0)row=true;
+                    if(j==0)col=true;
+                    a[i][0]=0;
+                    a[0][j]=0;
                 }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(a[i][j]==0){
+                    a[i][0]=0;
+                    a[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(a[i][0]==0 || a[0][j]==0){
+                    a[i][j]=0;
+                }
+            }
+        }
+        if(row){
+            for(int i=0;i<n;i++){
+                a[0][i]=0;
             }
         }
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if((row.find(i)!=row.end()|| col.find(j)!=col.end() )){
-                    matrix[i][j] = 0;
-                }
+        if(col){
+            for(int i=0;i<m;i++){
+                a[i][0]=0;
             }
         }
-        // return matrix;
     }
 };
