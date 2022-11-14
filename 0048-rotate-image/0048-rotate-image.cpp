2b1e42216de5fd100389bf303c268be9d5783df7
Nagsen Waghmare <nagsenwaghmare559@gmail.com>
2022-11-14 14:11:46 +0530

    Time: 10 ms (9.87%), Space: 7.2 MB (34.23%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<m;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+    }
+};
